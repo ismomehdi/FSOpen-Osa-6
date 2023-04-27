4355077631d5a4f92082c2e8733945a1d4a08913
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-04-27 18:39:34 +0300

    6.9 paremmat anekdootit, step7

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,15 @@ import { handleVote } from '../reducers/anecdoteReducer'
 import { useSelector, useDispatch } from "react-redux"
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({ filter, anecdotes }) => {
+        if (filter === '') {
+            return anecdotes
+        } else {
+            return anecdotes.filter(
+                anecdote => anecdote.content.toLowerCase()
+                .includes(filter.toLowerCase()))
+        }
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
